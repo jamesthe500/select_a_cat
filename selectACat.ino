ecbc4a7a9d2252c20aa31446a9b7f3b02e7726ff
jamesthe500 <jamesddaniels@gmail.com>
2015-11-21 12:01:11 -0600

    Demonstrates basic logic for opening and closing doors

--- selectACat.ino
+++ selectACat.ino
@@ -6,12 +6,12 @@
   3. A mic listens for that pitch or something quite close to it.
   4. When heard, a sensor looks for a sitting cat within a certain range...
   5. ... which triggers a servo to rotate the food bowl cover plates to the open position while an LED flashes the stays on for open.
-  6. The cat should bend down to eat now, this is sensed with a lower distance sensor & that the upper one senses nothing. 
+  6. The cat should bend down to eat now, this is sensed with a lower distance sensor & that the upper one senses nothing.
       (these sensors have LED feedback as well)
   7. if the cat doesn't bend down within 10 seconds, it closes (LED flashes).
   8. if the cat does, it stays open until it senses the cat has sat up, at which point it immediately closes (LED flashes).
   9. Neither closing nor opening will happen very quickly.
-  10. A protected button & app button will allow humans to trigger the opening and closing of the cover plates. 
+  10. A protected button & app button will allow humans to trigger the opening and closing of the cover plates.
       If the plates are locked open, all other functions can happen, just with a blinking LED instead of sliding plates.
 
   bowl-filler
@@ -34,14 +34,13 @@
   5. Control the setting of allowed daily maximum.
   6. Display any alerts when things don't work right.
 
-  pseudo-code
-
-
 */
 const int upperSensorPin = 8;
 const int servoPin = 13;
 const int lowerSensorPin = 4;
 const int micPin = 6;
+
+int bowlState = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -52,14 +51,8 @@ void setup() {
 }
 
 void loop() {
-  int upperSensorVal = digitalRead(upperSensorPin);
-  int lowerSensorVal = digitalRead(lowerSensorPin);
-  int meowReceived = digitalRead(micPin);
-  Serial.println(upperSensorVal);
-  Serial.println(lowerSensorVal);
-  Serial.println(meowReceived);
-  
-  if (upperSensorVal == LOW && lowerSensorVal == LOW && meowReceived == LOW) {
+
+  if (digitalRead(upperSensorPin) == LOW && digitalRead(lowerSensorPin) == LOW && digitalRead(micPin) == LOW) {
     // blinking represents the servo in the process of opening
     for (int i = 0; i < 40; i++) {
       digitalWrite(servoPin, HIGH);
@@ -68,13 +61,26 @@ void loop() {
       delay(50);
       i++;
     }
+    bowlState = 1;
     // light on means it's open
-    //digitalWrite(servoPin, HIGH);
-    //delay(10000); 
-    while (upperSensorVal == HIGH && lowerSensorVal == LOW) {
-      digitalWrite(servoPin, LOW);
+    digitalWrite(servoPin, HIGH);
+    delay(2000);
+  }
+  // PROBLEM: not entering this while loop because it is misreading the buttons as both being low,
+  // no mater what the buttons are doing IRL
+  // it's as if the bowlState affects these to make them down.
+  // If I remove the bowl state factor, it enters the while loop but won't exit.
+  // if I remove the preceeding delay, that has no affect.
+  // SOLVED: it didn't like the variables in place of digitalRead...
+
+  while (digitalRead(upperSensorPin) == HIGH && digitalRead(lowerSensorPin) == LOW && bowlState == 1) {
+    Serial.println("inWhile");
+
+    digitalWrite(servoPin, HIGH);
     }
-    // blinking represents the servo in the process of closing
+  
+  // blinking represents the servo in the process of closing
+  if (bowlState == 1) {
     for (int i = 0; i < 40; i++) {
       digitalWrite(servoPin, HIGH);
       delay(50);
@@ -82,12 +88,16 @@ void loop() {
       delay(50);
       i++;
     }
-    
-  } else {
-    digitalWrite(servoPin, LOW);
+    bowlState = 0;
+    Serial.println("bowl:");
+    Serial.println(bowlState);
   }
 
+  if (digitalRead(upperSensorPin) != LOW || digitalRead(lowerSensorPin) != LOW || digitalRead(micPin) != LOW) {
+    digitalWrite(servoPin, LOW);
+  }
 }
+
 
 
 
