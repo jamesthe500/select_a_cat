286f0d233db56ee191e956c7cce9ab5520194ace
jamesthe500 <jamesddaniels@gmail.com>
2015-11-23 01:43:49 -0600

    adds food dispensing logic

--- selectACat.ino
+++ selectACat.ino
@@ -6,35 +6,39 @@ const int servoPin = 13;
 const int catInRangePin = 4;
 const int micPin = 6;
 const int thereIsACatPin = 2;
+const int foodDispensorPin = 3;
+const int dispensorAtTopPin = 5;
 
 // later, make these pin assignments. Using them as simple constants now.
-const int selectorFactor0 = 0;
-const int selectorFactor1 = 0;
-const int selectorFactor2 = 0;
+const int selectorFactor0 = 1;
+const int selectorFactor1 = 1;
+const int selectorFactor2 = 1;
 const int selectorFactor3 = 1;
 const int selectorFactor4 = 0;
 
 // Varaibles
 int doorsOpen = 0;
-int ration = 0;
-int todaysFeed = 0;
+int ration = 3;
 int selectorPosition = 0;
-String stringOfPosition[8];
 
 void setup() {
   Serial.begin(9600);
   // Establich which pins are innies, which are outies
   pinMode(servoPin, OUTPUT);
   pinMode(thereIsACatPin, OUTPUT);
+  pinMode(foodDispensorPin, OUTPUT);
+  pinMode(dispensorAtTopPin, INPUT_PULLUP);
   pinMode(bowlWeightPin, INPUT_PULLUP);
   pinMode(catInRangePin, INPUT_PULLUP);
   pinMode(micPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // See where the selector is and change the daily ration as needed;
-  rationRead();
-  
+  // See where the selector is and change the ration as needed;
+
+  // un-comment when rotary switch is all hooked up.
+  // rationRead();
+
   // Because we're using digital PULLUP, LOW is HIGH and HIGH is LOW for all switches & sensors.
   // an LED indicates that the cat is close enough. This is more for the humans, but who knows? Cats can learn?
   if (digitalRead(catInRangePin) == LOW) {
@@ -84,15 +88,32 @@ void loop() {
 }
 
 void feed() {
-  // if there's not much food in the bowl, the daily limit of food hasn't been exceeded, and the doors are open, the hopper drops one portion of food.
-  if (doorsOpen == 1 && digitalRead(bowlWeightPin) == LOW && ration >= todaysFeed) {
-
+  // if there's not much food in the bowl and the doors are open, the hopper starts dropping portions of food.
+  if (doorsOpen == 1 && digitalRead(bowlWeightPin) == HIGH) {
+    // start rotating the motor. When it gets back to the top it checks weight (a higher value than above) and to see that it's under ration before continuing with the rotate.
+    Serial.println("inside if feed zone, ration:");
+    Serial.println(ration);
+    for (int i = 0 ; doorsOpen == 1 && digitalRead(bowlWeightPin) == HIGH && ration > i ; i++) {
+      // dispensor at top will be determined by a contact to a metal plate on a disc that rotates with the dispensor ball,
+      // so when it hits that plate it'll be oriented to the fill position. When it starts back up, it'll still be in contact for a bit
+      Serial.println("inside of feed zone");
+      while (digitalRead(dispensorAtTopPin) == LOW) {
+        // rotates out of the plate zone
+        digitalWrite(foodDispensorPin, HIGH);
+      }
+      while (digitalRead(dispensorAtTopPin) == HIGH) {
+        // keeps rotating until it gets back to the plate.
+        digitalWrite(foodDispensorPin, HIGH);
+      }
+    }
+    digitalWrite(foodDispensorPin, LOW);
   }
 }
-
+// uses binary to give each input from the rotary switch a unique value to add to the int, sets ration acordingly. This is mapped out, see the red grid notebook.
+// straight up is 1, going clockwise from there. The flat part of the shaft is what was used to idicate direction.
 void rationRead() {
   selectorPosition = (selectorFactor0 | selectorFactor1 * 2 | selectorFactor2 * 4 | selectorFactor3 * 8 | selectorFactor4 * 16);
-  
+  Serial.println("selector position value, then ration:");
   Serial.println(selectorPosition);
   switch (selectorPosition) {
     case 30:
@@ -104,7 +125,7 @@ void rationRead() {
   }
   switch (selectorPosition) {
     case 15:
-      Serial.println("selected 11110");
+      Serial.println("selected 01111");
       delay(1000);
       ration = 2;
       Serial.println(ration);
@@ -112,48 +133,48 @@ void rationRead() {
   }
   switch (selectorPosition) {
     case 31:
-      Serial.println("selected 01111");
-      delay(1000);    
+      Serial.println("selected 11110");
+      delay(1000);
       ration = 3;
       Serial.println(ration);
       break;
   }
-    switch (selectorPosition) {
+  switch (selectorPosition) {
     case 32:
       Serial.println("selected 11111");
-      delay(1000);    
+      delay(1000);
       ration = 4;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 8:
-      Serial.println("selected 00010");
-      delay(1000);    
+      Serial.println("selected 01000");
+      delay(1000);
       ration = 5;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 9:
-      Serial.println("selected 10010");
-      delay(1000);    
+      Serial.println("selected 01001");
+      delay(1000);
       ration = 6;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 24:
-      Serial.println("selected 00011");
-      delay(1000);    
+      Serial.println("selected 11000");
+      delay(1000);
       ration = 7;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 25:
-      Serial.println("selected 10011");
-      delay(1000);    
+      Serial.println("selected 11001");
+      delay(1000);
       ration = 8;
       Serial.println(ration);
       break;
@@ -161,23 +182,23 @@ void rationRead() {
   switch (selectorPosition) {
     case 10:
       Serial.println("selected 10101");
-      delay(1000);    
+      delay(1000);
       ration = 9;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 11:
-      Serial.println("selected 11010");
-      delay(1000);    
+      Serial.println("selected 01011");
+      delay(1000);
       ration = 10;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 26:
-      Serial.println("selected 01011");
-      delay(1000);    
+      Serial.println("selected 11010");
+      delay(1000);
       ration = 11;
       Serial.println(ration);
       break;
@@ -185,39 +206,39 @@ void rationRead() {
   switch (selectorPosition) {
     case 27:
       Serial.println("selected 11011");
-      delay(1000);    
+      delay(1000);
       ration = 12;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 12:
-      Serial.println("selected 00110");
-      delay(1000);    
+      Serial.println("selected 01100");
+      delay(1000);
       ration = 13;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 14:
-      Serial.println("selected 10110");
-      delay(1000);    
+      Serial.println("selected 01101");
+      delay(1000);
       ration = 14;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 29:
-      Serial.println("selected 00111");
-      delay(1000);    
+      Serial.println("selected 11100");
+      delay(1000);
       ration = 3;
       Serial.println(ration);
       break;
   }
   switch (selectorPosition) {
     case 30:
-      Serial.println("selected 10111");
-      delay(1000);    
+      Serial.println("selected 11101");
+      delay(1000);
       ration = 3;
       Serial.println(ration);
       break;
